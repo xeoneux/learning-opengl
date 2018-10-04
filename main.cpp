7887c45027b639bd302a3d3068eafca01cc4372a
Aayush Kapoor <xeoneux@icloud.com>
2018-10-04 10:50:09 +0530

    Create a context and a window

--- main.cpp
+++ main.cpp
@@ -7,11 +7,12 @@ using namespace std;
 
 int main() {
   GLFWwindow *window;
+  int w = 1024, h = 768;
 
   if (!glfwInit())
     exit(EXIT_FAILURE);
 
-  window = glfwCreateWindow(1024, 768, "glfw", nullptr, nullptr);
+  window = glfwCreateWindow(w, h, "glfw", nullptr, nullptr);
 
   if (!window) {
     glfwTerminate();
@@ -19,6 +20,20 @@ int main() {
   }
 
   glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glViewport(0, 0, w, h);
+    glClearColor(1, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Draw Code
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
 
   // Get Version String
   const GLubyte *p = glGetString(GL_VERSION);
