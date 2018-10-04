42814cae0ca80228da5cae707df323c5020ade64
Aayush Kapoor <xeoneux@icloud.com>
2018-10-04 10:32:38 +0530

    Print out OpenGL version

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include <stdlib.h>
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  GLFWwindow *window;
+
+  if (!glfwInit())
+    exit(EXIT_FAILURE);
+
+  window = glfwCreateWindow(1024, 768, "glfw", nullptr, nullptr);
+
+  if (!window) {
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  glfwMakeContextCurrent(window);
+
+  // Get Version String
+  const GLubyte *p = glGetString(GL_VERSION);
+  cout << "OpenGL Version: " << p << endl;
   return 0;
 }
