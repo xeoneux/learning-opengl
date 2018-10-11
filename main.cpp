50f1fd856370cb0b4e2049d225eca71d046b653e
Aayush Kapoor <xeoneux@icloud.com>
2018-10-11 17:59:10 +0530

    Export macros to separate file

--- main.cpp
+++ main.cpp
@@ -1,29 +1,7 @@
-#include "./include/glfw_app.hpp"
+#include "gl_macros.h"
+#include "glfw_app.hpp"
 
 #include <vector>
-
-// macro to write shader programs inline
-#ifndef GLSL
-#define GLSL(version, A) "#version " #version "\n" #A
-#endif
-
-// macro to pick the right glGenVertexArray function
-#ifndef GENVERTEXARRAYS
-#define GENVERTEXARRAYS(n, id)                                                 \
-  if (GLEW_APPLE_vertex_array_object)                                          \
-    glGenVertexArraysAPPLE(1, id);                                             \
-  else if (GLEW_ARB_vertex_array_object)                                       \
-  glGenVertexArrays(n, id)
-#endif
-
-// macro to pick the right glBindVertexArray function
-#ifndef BINDVERTEXARRAY
-#define BINDVERTEXARRAY(id)                                                    \
-  if (GLEW_APPLE_vertex_array_object)                                          \
-    glBindVertexArrayAPPLE(id);                                                \
-  else if (GLEW_ARB_vertex_array_object)                                       \
-  glBindVertexArray(id)
-#endif
 
 using namespace user;
 
@@ -208,7 +186,7 @@ struct MyApp : public App {
     BINDVERTEXARRAY(0);
   }
 
-  void onDraw() {
+  void onDraw() override {
     glUseProgram(sID);
     BINDVERTEXARRAY(arrayID);
     glDrawArrays(GL_TRIANGLES, 0, 3);
