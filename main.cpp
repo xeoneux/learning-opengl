3fbcf301e790c1825319ab77ce75215bb79391e8
Aayush Kapoor <xeoneux@icloud.com>
2018-10-11 18:12:55 +0530

    Create new shader class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+#include <vector>
+
 #include "gl_macros.h"
 #include "glfw_app.hpp"
 
-#include <vector>
+#include "gl_shader.hpp"
 
 using namespace user;
 
@@ -19,46 +21,6 @@ const char *vert = GLSL(120, attribute vec4 position; attribute vec4 color;
 const char *frag = GLSL(120, varying vec4 dstColor;
 
                         void main() { gl_FragColor = dstColor; });
-
-/*-----------------------------------------------------------------------------
- *  FUNCTION TO CHECK FOR SHADER COMPILER ERRORS
- *-----------------------------------------------------------------------------*/
-void shaderCompilerCheck(GLuint ID) {
-  GLint comp;
-  glGetShaderiv(ID, GL_COMPILE_STATUS, &comp);
-
-  if (comp == GL_FALSE) {
-    cout << "Shader Compilation FAILED" << endl;
-    GLchar messages[256];
-    glGetShaderInfoLog(ID, sizeof(messages), 0, &messages[0]);
-    cout << messages;
-  }
-}
-
-/*-----------------------------------------------------------------------------
- *  FUNCTION TO CHECK FOR SHADER LINK ERRORS
- *-----------------------------------------------------------------------------*/
-void shaderLinkCheck(GLuint ID) {
-  GLint linkStatus, validateStatus;
-  glGetProgramiv(ID, GL_LINK_STATUS, &linkStatus);
-
-  if (linkStatus == GL_FALSE) {
-    cout << "Shader Linking FAILED" << endl;
-    GLchar messages[256];
-    glGetProgramInfoLog(ID, sizeof(messages), 0, &messages[0]);
-    cout << messages;
-  }
-
-  glValidateProgram(ID);
-  glGetProgramiv(ID, GL_VALIDATE_STATUS, &validateStatus);
-
-  if (linkStatus == GL_FALSE) {
-    cout << "Shader Validation FAILED" << endl;
-    GLchar messages[256];
-    glGetProgramInfoLog(ID, sizeof(messages), 0, &messages[0]);
-    cout << messages;
-  }
-}
 
 /*-----------------------------------------------------------------------------
  *  CREATE A PLAIN-OLD-DATA ("POD") Container for 2D Coordinates
@@ -93,8 +55,8 @@ struct MyApp : public App {
   // A Container for Vertices
   vector<Vertex> triangle;
 
-  // ID of shader
-  GLuint sID;
+  // Shader
+  Shader shader = Shader(vert, frag);
 
   // ID of Vertex Attribute
   GLuint positionID, colorID;
@@ -105,7 +67,6 @@ struct MyApp : public App {
   MyApp() { init(); }
 
   void init() {
-
     // Specify the 3 VERTICES of A Triangle
     Vertex v1 = {vec2(-1, -.5), vec4(1, 0, 0, 1)};
     Vertex v2 = {vec2(0, 1), vec4(0, 1, 0, 1)};
@@ -114,44 +75,10 @@ struct MyApp : public App {
     triangle.push_back(v2);
     triangle.push_back(v3);
 
-    /*-----------------------------------------------------------------------------
-     *  CREATE THE SHADER
-     *-----------------------------------------------------------------------------*/
+    positionID = glGetAttribLocation(shader.id(), "position");
+    colorID = glGetAttribLocation(shader.id(), "color");
 
-    // 1. CREATE SHADER PROGRAM
-    sID = glCreateProgram();
-    GLuint vID = glCreateShader(GL_VERTEX_SHADER);
-    GLuint fID = glCreateShader(GL_FRAGMENT_SHADER);
-
-    // 2. LOAD SHADER SOURCE CODE
-    glShaderSource(vID, 1, &vert, NULL);
-    glShaderSource(fID, 1, &frag, NULL);
-
-    // 3. COMPILE
-    glCompileShader(vID);
-    glCompileShader(fID);
-
-    // 4. CHECK FOR COMPILE ERRORS
-    shaderCompilerCheck(vID);
-    shaderCompilerCheck(fID);
-
-    // 5. ATTACH SHADERS TO PROGRAM
-    glAttachShader(sID, vID);
-    glAttachShader(sID, fID);
-
-    // 6. LINK PROGRAM
-    glLinkProgram(sID);
-
-    // 7. CHECK FOR LINKING ERRORS
-    shaderLinkCheck(sID);
-
-    // 8. USE PROGRAM
-    glUseProgram(sID);
-
-    positionID = glGetAttribLocation(sID, "position");
-    colorID = glGetAttribLocation(sID, "color");
-
-    glUseProgram(0);
+    shader.unbind();
 
     /*-----------------------------------------------------------------------------
      *  CREATE THE VERTEX ARRAY OBJECT
@@ -187,11 +114,11 @@ struct MyApp : public App {
   }
 
   void onDraw() override {
-    glUseProgram(sID);
+    shader.bind();
     BINDVERTEXARRAY(arrayID);
     glDrawArrays(GL_TRIANGLES, 0, 3);
     BINDVERTEXARRAY(0);
-    glUseProgram(0);
+    shader.unbind();
   }
 
   void onMouseMove(int x, int y) override { cout << x << " " << y << endl; }
