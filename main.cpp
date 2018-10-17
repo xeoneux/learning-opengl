ebc1f5f5f694102d94bf6adba6c6ebf3692208ff
Aayush Kapoor <xeoneux@icloud.com>
2018-10-17 13:57:21 +0530

    Scale, Rotate & Transform

--- main.cpp
+++ main.cpp
@@ -1,50 +1,45 @@
-#include <vector>
 
-#include "gl_macros.h"
+
 #include "glfw_app.hpp"
-
 #include "gl_shader.hpp"
+#include "gl_macros.hpp"
+
+#define GLM_FORCE_RADIANS
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
 using namespace user;
 
 /*-----------------------------------------------------------------------------
  *  SOME SHADER CODE
  *-----------------------------------------------------------------------------*/
-const char *vert = GLSL(120, attribute vec4 position; attribute vec4 color;
-                        varying vec4 dstColor;
+const char *vert = GLSL(
+    120,
 
-                        void main() {
-                          dstColor = color;
-                          gl_Position = position;
-                        });
+    attribute vec4 position;
+    attribute vec4 color;
 
-const char *frag = GLSL(120, varying vec4 dstColor;
+    varying vec4 dstColor;
 
-                        void main() { gl_FragColor = dstColor; });
+    uniform mat4 model; uniform mat4 view; uniform mat4 projection;
 
-/*-----------------------------------------------------------------------------
- *  CREATE A PLAIN-OLD-DATA ("POD") Container for 2D Coordinates
- *-----------------------------------------------------------------------------*/
-struct vec2 {
-  vec2(float _x = 0, float _y = 0) : x(_x), y(_y) {}
-  float x, y;
-};
+    void main() {
+      dstColor = color;
+      gl_Position = projection * view * model * position;
+    }
 
-/*-----------------------------------------------------------------------------
- *  CREATE A PLAIN-OLD-DATA ("POD") Container for RGBA values
- *-----------------------------------------------------------------------------*/
-struct vec4 {
-  vec4(float _r = 1, float _g = 1, float _b = 1, float _a = 1)
-      : r(_r), g(_g), b(_b), a(_a) {}
-  float r, g, b, a;
-};
+);
+
+const char *frag =
+    GLSL(120, varying vec4 dstColor; void main() { gl_FragColor = dstColor; });
 
 /*-----------------------------------------------------------------------------
  *  CREATE A VERTEX OBJECT
  *-----------------------------------------------------------------------------*/
 struct Vertex {
-  vec2 position;
-  vec4 color;
+  glm::vec2 position;
+  glm::vec4 color;
 };
 
 /*-----------------------------------------------------------------------------
@@ -52,33 +47,37 @@ struct Vertex {
  *-----------------------------------------------------------------------------*/
 struct MyApp : public App {
 
-  // A Container for Vertices
-  vector<Vertex> triangle;
-
-  // Shader
-  Shader shader = Shader(vert, frag);
-
+  Shader *shader;
   // ID of Vertex Attribute
   GLuint positionID, colorID;
   // A buffer ID
-  GLuint arrayID, bufferID;
+  GLuint bufferID;
+  // An array ID
+  GLuint arrayID;
+  // ID of Uniform
+  GLuint modelID, viewID, projectionID;
 
   // Constructor (initialize application)
-  MyApp() { init(); }
+  MyApp() : App() { init(); }
 
   void init() {
+
     // Specify the 3 VERTICES of A Triangle
-    Vertex v1 = {vec2(-1, -.5), vec4(1, 0, 0, 1)};
-    Vertex v2 = {vec2(0, 1), vec4(0, 1, 0, 1)};
-    Vertex v3 = {vec2(1, -.5), vec4(0, 0, 1, 1)};
-    triangle.push_back(v1);
-    triangle.push_back(v2);
-    triangle.push_back(v3);
+    Vertex triangle[] = {{glm::vec2(-1, 0), glm::vec4(1, 0, 0, 1)},
+                         {glm::vec2(0, 1), glm::vec4(0, 1, 0, 1)},
+                         {glm::vec2(1, 0), glm::vec4(0, 0, 1, 1)}};
 
-    positionID = glGetAttribLocation(shader.id(), "position");
-    colorID = glGetAttribLocation(shader.id(), "color");
+    // 1. CREATE SHADER PROGRAM
+    shader = new Shader(vert, frag);
 
-    shader.unbind();
+    // Get attribute locations
+    positionID = glGetAttribLocation(shader->id(), "position");
+    colorID = glGetAttribLocation(shader->id(), "color");
+
+    // Get uniform locations
+    modelID = glGetUniformLocation(shader->id(), "model");
+    viewID = glGetUniformLocation(shader->id(), "view");
+    projectionID = glGetUniformLocation(shader->id(), "projection");
 
     /*-----------------------------------------------------------------------------
      *  CREATE THE VERTEX ARRAY OBJECT
@@ -94,8 +93,7 @@ struct MyApp : public App {
     // Bind Array Buffer
     glBindBuffer(GL_ARRAY_BUFFER, bufferID);
     // Send data over buffer to GPU
-    glBufferData(GL_ARRAY_BUFFER, triangle.size() * sizeof(Vertex),
-                 &(triangle[0]), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(Vertex), triangle, GL_STATIC_DRAW);
 
     /*-----------------------------------------------------------------------------
      *  ENABLE VERTEX ATTRIBUTES
@@ -108,34 +106,72 @@ struct MyApp : public App {
     // Tell OpenGL how to handle the buffer of data that is already on the GPU
     glVertexAttribPointer(positionID, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
     glVertexAttribPointer(colorID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex),
-                          (void *)sizeof(vec2));
+                          (void *)sizeof(glm::vec2));
 
     BINDVERTEXARRAY(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
   }
 
-  void onDraw() override {
-    shader.bind();
+  void onDraw() {
+
+    static float time = 0.0;
+    time += .01;
+
+    // Bind Shader
+    glUseProgram(shader->id());
+    // Bind Vertex Array Object
     BINDVERTEXARRAY(arrayID);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    /*-----------------------------------------------------------------------------
+     *  View Transform:    Eye Position    Target    Up Direction
+     *-----------------------------------------------------------------------------*/
+    glm::mat4 view =
+        glm::lookAt(glm::vec3(0, 0, 2), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+
+    /*-----------------------------------------------------------------------------
+     *  Projection Transform:    Field of View    Ratio    Near Far
+     *-----------------------------------------------------------------------------*/
+    // glm requires us to use floats!  use .f or else you'll get compiler errors
+    glm::mat4 proj =
+        glm::perspective(PI / 3.0f, window().ratio(), 0.1f, -10.0f);
+
+    /*-----------------------------------------------------------------------------
+     *  Send view and projection matrices to Shader Uniforms
+     *-----------------------------------------------------------------------------*/
+    glUniformMatrix4fv(viewID, 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(projectionID, 1, GL_FALSE, glm::value_ptr(proj));
+
+    for (int i = 0; i < 100; ++i) {
+
+      /*-----------------------------------------------------------------------------
+       *  Create transformation matrices
+       *-----------------------------------------------------------------------------*/
+      glm::mat4 translate =
+          glm::translate(glm::mat4(), glm::vec3(sin(time), 0, (float)i / 100));
+      glm::mat4 rotate =
+          glm::rotate(glm::mat4(), time * PI * i / 100, glm::vec3(0, 0, 1));
+      glm::mat4 scale =
+          glm::scale(glm::mat4(), glm::vec3(1.0f - (float)i / 100));
+
+      /*-----------------------------------------------------------------------------
+       *  Scale, then Rotate, then Translate = translate * rotate * scale
+       *-----------------------------------------------------------------------------*/
+      glm::mat4 model = translate * rotate * scale;
+
+      glUniformMatrix4fv(modelID, 1, GL_FALSE, glm::value_ptr(model));
+      glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
+
+    // Unbind Vertex Array Object and Shader
     BINDVERTEXARRAY(0);
-    shader.unbind();
-  }
-
-  void onMouseMove(int x, int y) override { cout << x << " " << y << endl; }
-
-  void onMouseDown(int button, int action) override {
-    if (action == GLFW_PRESS)
-      cout << "mouse pressed" << endl;
-    if (action == GLFW_RELEASE)
-      cout << "mouse released" << endl;
-  }
-
-  void onKeyDown(int key, int action) override {
-    cout << (unsigned char)key << endl;
+    glUseProgram(0);
   }
 };
 
-int main() {
+int main(int argc, const char **argv) {
+
   MyApp app;
   app.start();
+
+  return 0;
 }
